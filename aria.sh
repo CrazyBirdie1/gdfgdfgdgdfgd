75b7c369fb7881306369a240e3916e43db5cde58
CrazyBirdie1 <danielwoo394@gmail.com>
2020-09-04 09:41:24 +0800

    Update aria.sh

--- aria.sh
+++ aria.sh
@@ -1,9 +1,9 @@
 export MAX_DOWNLOAD_SPEED=0
 tracker_list=$(curl -Ns https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt | awk '$1' | tr '\n' ',')
-export MAX_CONCURRENT_DOWNLOADS=4
+export MAX_CONCURRENT_DOWNLOADS=7
 aria2c --enable-rpc --rpc-listen-all=false --rpc-listen-port 6800 --check-certificate=false\
    --max-connection-per-server=10 --rpc-max-request-size=1024M \
-   --bt-tracker="[$tracker_list]" --bt-max-peers=0 --bt-stop-timeout=600 --bt-tracker-connect-timeout=600 --seed-time=0.01 --min-split-size=10M --peer-id-prefix=-TR2770- --user-agent=Transmission/2.77 \
+   --bt-tracker="[$tracker_list]" --bt-max-peers=0 --bt-stop-timeout=600 --bt-tracker-connect-timeout=600 --seed-time=0.01 --min-split-size=10M --peer-id-prefix=-qB4201- --user-agent=qBittorrent/4.2.1 \
    --follow-torrent=mem --split=10 \
    --daemon=true --allow-overwrite=true --max-overall-download-limit=$MAX_DOWNLOAD_SPEED \
    --max-overall-upload-limit=1K --max-concurrent-downloads=$MAX_CONCURRENT_DOWNLOADS
